3a4c7978ff9dbd4d5cd4e0f693fa32ca950dcb54
diaros00 <63287535+diaros00@users.noreply.github.com>
2024-02-01 12:53:56 +0700

    ok

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ import express from 'express'
 import chalk from 'chalk'
 import Debug from 'debug'
 import morgan from 'morgan'
+import path from 'path'
+import { fileURLToPath } from '/public/';
+
 const debug = Debug('app')
 const app = express()
 const port = 3000
 
 app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname, '/public/')))
 
 app.get('/', (req, res) => {
   res.send('Hello born to dev. saveasdfasdf11')
