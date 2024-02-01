f4d759f9eb443ca2249593ac49c5d72a36291802
diaros00 <63287535+diaros00@users.noreply.github.com>
2024-02-01 13:04:51 +0700

    upload theme

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-import express from 'express'
-import chalk from 'chalk'
-import Debug from 'debug'
-import morgan from 'morgan'
-import path from 'path'
-import { fileURLToPath } from '/public/';
+const express = require('express')
+const chalk = require('chalk')
+const debug = require('debug')('app')
+const morgan = require('morgan')
+const path = require('path')
+// import { fileURLToPath } from '/public/';
 
-const debug = Debug('app')
 const app = express()
 const port = 3000
 
@@ -13,7 +12,7 @@ app.use(morgan('combined'))
 app.use(express.static(path.join(__dirname, '/public/')))
 
 app.get('/', (req, res) => {
-  res.send('Hello born to dev. saveasdfasdf11')
+  res.send('Hello born to dev.')
 })
 
 app.listen(port, () => {
