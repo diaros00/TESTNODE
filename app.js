f0e5446d55c15b3438906ad45095d89037f6cbee
diaros00 <63287535+diaros00@users.noreply.github.com>
2024-02-02 13:15:08 +0700

    rounting

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const chalk = require('chalk')
 const debug = require('debug')('app')
 const morgan = require('morgan')
 const path = require('path')
+const filesRounter = express.Router()
+const accountRounter = express.Router()
 // import { fileURLToPath } from '/public/';
 
 const app = express()
@@ -13,6 +15,22 @@ app.use(express.static(path.join(__dirname, '/public/')))
 
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
+
+filesRounter.route('/').get((req, res) => {
+  res.render('files', {
+    username: 'Chatraporn.Yon',
+  })
+})
+
+app.use('/files', filesRounter)
+
+accountRounter.route('/').get((req, res) => {
+  res.render('account', {
+    username: 'Chatraporn.Yon',
+  })
+})
+
+app.use('/account', accountRounter)
 
 app.get('/', (req, res) => {
   res.render('index', {
